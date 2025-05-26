cc21bd01a8fe7e886e436ce3a93217b9ef510a9a
Ricky Rozian <rrozian00@gmail.com>
2025-05-26 21:51:11 +0700

    undangan5

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
-// Countdown logic
-const countdownElement = document.getElementById("countdown");
-const weddingDate = new Date("2025-09-07T09:00:00").getTime();
+const btn = document.getElementById("bukaUndangan");
+const musik = document.getElementById("music");
+const splash = document.getElementById("splash");
+const card = document.getElementById("card");
 
-function updateCountdown() {
-  const now = new Date().getTime();
-  const distance = weddingDate - now;
-
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hrs = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const secs = Math.floor((distance % (1000 * 60)) / 1000);
-
-  countdownElement.innerHTML = `${days} Hari ${hrs} Jam ${mins} Menit ${secs} Detik`;
-
-  if (distance < 0) {
-    clearInterval(timer);
-    countdownElement.innerHTML = "Acara telah berlangsung";
-  }
-}
-
-const timer = setInterval(updateCountdown, 1000);
+btn.addEventListener("click", () => {
+  splash.style.display = "none";
+  musik.muted = false;
+  musik.play().catch((e) => {
+    console.log("Gagal memutar musik:", e);
+  });
+});
